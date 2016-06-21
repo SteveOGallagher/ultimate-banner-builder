ae4674ceeb08639e137d73badba52b226f7654c8
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2016-06-21 15:55:19 +0100

    Create local GDN object to mimic DoubleClick layout for use in main.js

--- base-template/GDN.js
+++ base-template/GDN.js
@@ -26,10 +26,12 @@ function exitHandler() {
 }
 
 // Wait for the DOM to load before initialising banner load
-if (document.addEventListener) {// For all major browsers, except IE 8 and earlier
-  document.addEventListener("DOMContentLoaded", politeInit);
-} else {
-  politeInit();
+function initialize() {
+  if (document.addEventListener) {// For all major browsers, except IE 8 and earlier
+    document.addEventListener("DOMContentLoaded", politeInit);
+  } else {
+    politeInit();
+  }
 }
 
 ///
@@ -39,16 +41,31 @@ if (document.addEventListener) {// For all major browsers, except IE 8 and earli
 function getDynamicContent() {
   /* Create your own version of the below object with local references */
   var devDynamicContent = {};
-  devDynamicContent = [{}];
-  devDynamicContent[0]._id = 0;
-  devDynamicContent[0].main_image.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43436498/dirty/bg-chloe-160x600.jpg";
-  devDynamicContent[0].image_url_1.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43412835/dirty/a-trip-160x600.png";
-  devDynamicContent[0].image_url_2.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43342233/dirty/prizes-160x600.png";
-  devDynamicContent[0].image_url_3.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43326971/dirty/find-out-more-160x600.png";
-  devDynamicContent[0].exit.Url = "http://www.google.com/";
-  Enabler.setDevDynamicContent(devDynamicContent);
+  devDynamicContent.GDN = [{
+    main_image : {
+      Url : "https://s0.2mdn.net/ads/richmedia/studio/pv2/43436498/dirty/bg-chloe-160x600.jpg"
+    },
+    image_url_1 : {
+      Url : "https://s0.2mdn.net/ads/richmedia/studio/pv2/43412835/dirty/a-trip-160x600.png"
+    },
+    image_url_2 : {
+      Url : "https://s0.2mdn.net/ads/richmedia/studio/pv2/43342233/dirty/prizes-160x600.png"
+    },
+    image_url_3 : {
+      Url : "https://s0.2mdn.net/ads/richmedia/studio/pv2/43326971/dirty/find-out-more-160x600.png"
+    },
+    exit : {
+      Url : "http://www.google.com/"
+    }
+
+  }];
+  // devDynamicContent.GDN[0].main_image.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43436498/dirty/bg-chloe-160x600.jpg";
+  // devDynamicContent.GDN[0].image_url_1.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43412835/dirty/a-trip-160x600.png";
+  // devDynamicContent.GDN[0].image_url_2.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43342233/dirty/prizes-160x600.png";
+  // devDynamicContent.GDN[0].image_url_3.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43326971/dirty/find-out-more-160x600.png";
+  // devDynamicContent.GDN[0].exit.Url = "http://www.google.com/";
   /* END OF GOOGLE DYNAMIC CODE SNIPPET */
 
   // Replace the below object variable with the one pasted above.
-  return devDynamicContent[0];
+  return devDynamicContent.GDN[0];
 }
