6662fed10b8243be563eeac611d7a7be48fd4bbd
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2016-06-21 16:04:51 +0100

    Fix null parent error in GDN.js. Remove unused data.json file.

--- base-template/GDN.js
+++ base-template/GDN.js
@@ -57,15 +57,6 @@ function getDynamicContent() {
     exit : {
       Url : "http://www.google.com/"
     }
-
   }];
-  // devDynamicContent.GDN[0].main_image.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43436498/dirty/bg-chloe-160x600.jpg";
-  // devDynamicContent.GDN[0].image_url_1.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43412835/dirty/a-trip-160x600.png";
-  // devDynamicContent.GDN[0].image_url_2.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43342233/dirty/prizes-160x600.png";
-  // devDynamicContent.GDN[0].image_url_3.Url = "https://s0.2mdn.net/ads/richmedia/studio/pv2/43326971/dirty/find-out-more-160x600.png";
-  // devDynamicContent.GDN[0].exit.Url = "http://www.google.com/";
-  /* END OF GOOGLE DYNAMIC CODE SNIPPET */
-
-  // Replace the below object variable with the one pasted above.
   return devDynamicContent.GDN[0];
 }
