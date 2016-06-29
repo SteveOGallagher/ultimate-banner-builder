408034c0a4fd53cd2ae54cbb581aad315f430492
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2016-06-29 09:32:19 +0100

    Simplify the layour of the content object.

--- base-template/image-paths.js
+++ base-template/image-paths.js
@@ -1,24 +1,20 @@
 function getContent() {
   /* Create your own version of the below object with local references */
+
   var devDynamicContent = {};
-  devDynamicContent.GDN = [{
-    main_image : {
-      Url : "img/red.jpg"
-    },
-    image_url_1 : {
-      Url : "img/blue.jpg"
-    },
-    image_url_2 : {
-      Url : "img/green.jpg"
-    },
-    image_url_3 : {
-      Url : "img/orange.jpg"
-    },
-    exit : {
-      Url : "http://www.google.com"
-    }
-  }];
-  return devDynamicContent.GDN[0];
+  devDynamicContent.GDN= [{}];
+  devDynamicContent.static[0].main_image = {};
+  devDynamicContent.static[0].main_image.Url = "img/red.jpg";
+  devDynamicContent.static[0].image_url_1 = {};
+  devDynamicContent.static[0].image_url_1.Url = "img/blue.jpg";
+  devDynamicContent.static[0].image_url_2 = {};
+  devDynamicContent.static[0].image_url_2.Url = "img/green.jpg";
+  devDynamicContent.static[0].image_url_3 = {};
+  devDynamicContent.static[0].image_url_3.Url = "img/orange.jpg";
+  devDynamicContent.static[0].exit = {};
+  devDynamicContent.static[0].exit.Url = "http://www.google.com/";
+
+  return devDynamicContent.static[0];
 }
 
 // TODO: Refactor the above to retain structure but add simplicity.
